8b6934825855325c6d9f6497b940149c4febfc5b
standarddeviant <dave.crist@gmail.com>
2026-06-16 23:53:15 -0400

    add yolo instructions

--- yolo.sh
+++ yolo.sh
@@ -1,14 +1,6 @@
-
-# mq-sandbox
-This is a basic example of using message queues for IPC on linux.
-
-# YOLO Instructions
-
-```
 git clone https://github.com/standarddeviant/mq-sandbox.git
 cd mq-sandbox
 ./build.sh
 ./receiver &
 sleep 2
 ./sender
-```
